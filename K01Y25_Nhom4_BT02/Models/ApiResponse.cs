89a4266c7c581159664026f538e3bd33563cb68b
phuoctm1910 <nothinginthis465@gmail.com>
2025-01-12 12:35:36 +0700

    Config return of api

---
+++ K01Y25_Nhom4_BT02/Models/ApiResponse.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace K01Y25_Nhom4_BT02.Models
+{
+    public class ApiResponse<T>
+    {
+        public int Code { get; set; } // 0: success, 1: fail
+        public string? Status { get; set; } // "success" hoặc "fail"
+        public T? Data { get; set; } // Dữ liệu trả về khi thành công
+        public string? Message { get; set; } // Thông báo lỗi khi thất bại
+
+
+        // Trường hợp thành công
+        public static ApiResponse<T> Success(T data, string? message)
+        {
+            return new ApiResponse<T>
+            {
+                Code = 0,
+                Status = "success",
+                Data = data,
+                Message = message
+            };
+        }
+
+
+        // Trường hợp thất bại
+        public static ApiResponse<T> Fail(string message)
+        {
+            return new ApiResponse<T>
+            {
+                Code = 1,
+                Status = "fail",
+                Data = default,
+                Message = message
+            };
+        }
+
+
+    }
+
+}
